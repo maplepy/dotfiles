69f5779b4f5d9a3b69933e3348fea6bc688bdf89
maple <github.register@slmail.me>
2023-10-27 15:17:19 +0200

    Update .config/qtile/config.py Update settings.json

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -25,74 +25,13 @@ dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
-
-# floating_layout = layout.Floating(
-# 	float_rules=[
-# 		# Run the utility of `xprop` to see the wm class and name of an X client.
-# 		*layout.Floating.default_float_rules,
-# 		Match(wm_class="confirmreset"),  # gitk
-# 		Match(wm_class="makebranch"),  # gitk
-# 		Match(wm_class="maketag"),  # gitk
-# 		Match(wm_class="ssh-askpass"),  # ssh-askpass
-# 		Match(title="branchdialog"),  # gitk
-# 		Match(title="pinentry"),  # GPG key password entry
-# 	]
-# )
-
-from libqtile import bar, widget
-from libqtile.config import Screen
-
-# widget_defaults = dict(
-# 	font="JetBrainsMonoNL Nerd Font Propo",
-# 	fontsize=14,
-# 	padding=2,
-# )
-
-# screens = [
-# 	Screen(
-# 		wallpaper="~/.local/wallpapers/evening-sky-flipped.png",
-# 		wallpaper_mode="fill",
-# 		top=bar.Bar([
-# 				widget.CurrentLayout(),
-# 				widget.GroupBox(),
-# 				widget.Prompt(),
-# 				widget.WindowName(),
-# 				widget.Battery(
-# 					discharge_char="v",
-# 					format="{char} {percent:2.0%} {hour:d}:{min:02d}",
-# 					notify_below=20,
-# 					low_percentage=0.2,
-# 					low_foreground="#ff0000",
-# 					),# display the battery state
-# 				widget.Chord(
-# 					chords_colors={
-# 						"launch": ("#ff0000", "#ffffff"),
-# 					},
-# 					name_transform=lambda name: name.upper(),
-# 				),
-# 				# widget.TextBox("default config", name="default"),
-# 				# widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-# 				# NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-# 				# widget.StatusNotifier(),
-# 				widget.Systray(),
-# 				widget.Clock(format="%Y-%m-%d %a %H:%M"),
-# 				# widget.QuickExit(),
-# 			],
-# 			20,
-# 			# border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-# 			# border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
-# 		),
-# 	),
-# ]
-
-
 auto_fullscreen = True
 focus_on_window_activation = "smart"
 reconfigure_screens = True
 
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
-auto_minimize = True
+auto_minimize = False
 
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
