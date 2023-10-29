a8564223e1bc3662a884072e12f07166099fd995
maple <github.register@slmail.me>
2023-10-30 00:04:42 +0100

    Update .config/qtile/config.py

--- dot_config/qtile/config.py
+++ dot_config/qtile/config.py
@@ -22,16 +22,16 @@ from core import hooks
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
-follow_mouse_focus = True
-bring_front_click = False
-cursor_warp = False
-auto_fullscreen = True
+follow_mouse_focus = True # Focus follows mouse
+bring_front_click = False # Bring windows to front when clicked
+cursor_warp = False # Warp the mouse cursor to the center of the focused window
 focus_on_window_activation = "smart"
-reconfigure_screens = True
+reconfigure_screens = True # Reconfigure screens when screens are added/removed
 
 # If things like steam games want to auto-minimize themselves when losing
 # focus, should we respect this or not?
-auto_minimize = False
+auto_minimize = True
+auto_fullscreen = True
 
 # When using the Wayland backend, this can be used to configure input devices.
 wl_input_rules = None
