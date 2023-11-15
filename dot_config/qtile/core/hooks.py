9355330c2ef347452c7d521a1b0a85009c60076d
maple <github.register@slmail.me>
2023-11-15 23:57:29 +0100

    Update .config/qtile/core/hooks.py

--- dot_config/qtile/core/hooks.py
+++ dot_config/qtile/core/hooks.py
@@ -43,8 +43,8 @@ def run_at_startup():
 # def resume():
 #     send_notification("qtile", "Resumed")
 
-# @hook.subscribe.client_new
-# async def client_new(client):
-#     await asyncio.sleep(0.5)
-#     if client.name == "Spotify":
-#         client.togroup("0")
+@hook.subscribe.client_new
+async def client_new(client):
+    await asyncio.sleep(0.5)
+    if client.name == "Spotify":
+        client.togroup("0")
