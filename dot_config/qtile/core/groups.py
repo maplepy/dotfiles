ff19369c872c1527f34c897aea420bc5d4b0b60e
maple <github.register@slmail.me>
2024-02-29 17:11:43 +0100

    Update .config/qtile/core/groups.py

--- dot_config/qtile/core/groups.py
+++ dot_config/qtile/core/groups.py
@@ -22,7 +22,7 @@ groups: list[Group] = []
 
 for key, label, layout, matches in [
 	("1", "", None, wm_class("VSCodium", "code", "jetbrains-clion")),
-	("2", "󰈹", None, wm_class("firefox", "LibreWolf", "Thorium-browser", "mercury-default")),
+	("2", "󰈹", None, wm_class("firefox", "LibreWolf", "Thorium-browser", "mercury-default", "floorp")),
 	("3", "", None, wm_class("FreeTube", "mpv")),
 	("4", "󰄄", None, wm_class("obsidian", "resolve", "darktable", "Rapid Photo Downloader", "krita")),
 	("5", "󰇮", None, wm_class("Signal", "discord", "VencordDesktop", "telegram-desktop")),
