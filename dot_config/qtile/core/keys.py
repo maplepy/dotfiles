7e56334a2750c24dc6ad55e000c846ccad166e7d
maple <github.register@slmail.me>
2024-02-29 17:11:00 +0100

    Update .config/qtile/core/keys.py Update settings.json

--- dot_config/qtile/core/keys.py
+++ dot_config/qtile/core/keys.py
@@ -13,8 +13,8 @@ app_menu = "rofi -modi run,drun,window -show drun -sidebar-mode -show-icons"
 emojis_menu = "rofi -modi emoji -show emoji -sidebar-mode -show-icons"
 terminal = "kitty"
 terminal2 = "alacritty"
-browser = "mercury-browser -p yep"
-browser2 = "thorium-browser"
+browser = "floorp"
+browser2 = "mercury-browser -p yep"
 
 @lazy.function
 def float_to_front(qtile) -> None:
