cc06cc71402bc7e26f3945427f82baa6f0290343
maple <github.register@slmail.me>
2024-06-13 01:20:39 +0200

    Update .config/qtile/extras/__init__.py

--- dot_config/qtile/extras/__init__.py
+++ dot_config/qtile/extras/__init__.py
@@ -1,3 +1,4 @@
+# Import necessary modules from qtile_extras
 from qtile_extras import widget  # type: ignore
 from qtile_extras.widget import modify  # type: ignore
 from qtile_extras.widget.decorations import (  # type: ignore
@@ -6,23 +7,11 @@ from qtile_extras.widget.decorations import (  # type: ignore
     RectDecoration,
 )
 
+# Import custom widgets and functions from extras
 from extras.clock import Clock
 from extras.groupbox import GroupBox
 from extras.misc import float_to_front
 from extras.textbox import TextBox
 from extras.battery import UPowerWidget
-from extras.network import WiFiIcon 
-
-__all__ = [
-    "BorderDecoration",
-    "Clock",
-    "float_to_front",
-    "GroupBox",
-    "UPowerWidget",
-    "WiFiIcon",
-    "modify",
-    "PowerLineDecoration",
-    "RectDecoration",
-    "TextBox",
-    "widget",
-]
+from extras.network import WiFiIcon
+from extras.mic import MicWidget
