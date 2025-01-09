919dc06ed0dc2dfa7463df05cf7648b34cd4f457
maple <github.register@slmail.me>
2025-01-09 22:25:45 +0100

    Update .config/qtile/core/groups.py

--- dot_config/qtile/core/groups.py
+++ dot_config/qtile/core/groups.py
@@ -28,7 +28,7 @@ for key, label, layout, matches in [
 	("5", "󰇮", None, wm_class("Signal", "discord", "vesktop", "telegram-desktop")),
 	("6", "󱚟", None, wm_class("SDConsole", "ComfyUI")),
 	("8", "󰊗", None, wm_class("Project Zomboid", "gmod", "cs2")),
-	("9", "󰓓", None, wm_class("steam", "Lutris", "debris.exe", "blacksmith.exe", "PrismLauncher")),
+	("9", "󰓓", None, wm_class("steam", "Lutris", "debris.exe", "blacksmith.exe", "PrismLauncher", "Dofus.x64")),
 	("0", "", None, wm_class("spotify")),
 ]:
 	groups.append(Group(key, matches, label=label, layout=layout))
