514a688b6edca4e2a319567416070c0183aeaa11
maplepy <github.register@slmail.me>
2025-10-30 15:02:46 +0000

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -202,19 +202,21 @@ function man --description "Colorize man pages"
 end
 
 # Jump
-function __jump_add --on-variable PWD
-  status --is-command-substitution; and return
-  jump chdir
-end
+if command -q jump
+  function __jump_add --on-variable PWD
+    status --is-command-substitution; and return
+    jump chdir
+  end
 
-function __jump_hint
-  set -l term (string replace -r '^j ' '' -- (commandline -cp))
-  jump hint "$term"
-end
+  function __jump_hint
+    set -l term (string replace -r '^j ' '' -- (commandline -cp))
+    jump hint "$term"
+  end
 
-function j
-  set -l dir (jump cd "$argv")
-  test -d "$dir"; and cd "$dir"
+  function j
+    set -l dir (jump cd "$argv")
+    test -d "$dir"; and cd "$dir"
+  end
 end
 
 complete --command j --exclusive --arguments '(__jump_hint)'
